3692899a3e078b35b13c59ecb086f5d730fc5efd
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-05 04:43:34 +0530

    Day 3 code

---
+++ day003.py
@@ -0,0 +1,26 @@
+#Day 3 - Aditi Sai Sharma - June 5, 2026
+#CS50P HARVARD (YT)
+
+#Program 1 - greetings
+print("hola amigo")
+
+#Program 2 -
+name = input("what is your name? ")
+print("hello, ", name)
+
+#Program 3 - Remove white spaces from str
+name = name.strip()
+
+#Program 4 - Capitalize first letter
+name = name.capitalize()
+
+#Programc 5 - Capitalize all 1st letters
+name = name.title()
+
+#Program 6 - altogether
+place = input("where do you live? ").strip().title()
+
+#Program 7 - split first, middle, last names
+first, middle, last = name.split(" ")
+print("hello, ", middle)
+
